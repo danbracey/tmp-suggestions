c47330f1144d4b22b8a441c0a64c5f2993c14aa3
Penguin <penguingeek1@gmail.com>
2023-09-24 21:35:35 +0100

    Add voting icons

--- resources/views/components/tables/suggestions.blade.php
+++ resources/views/components/tables/suggestions.blade.php
@@ -11,6 +11,17 @@
             <td>{{$Suggestion->getVotes()->count('vote')}}</td>
             <td>{{$Suggestion->getSubmitter->name}}</td>
             <td><a href="{{route('suggestion.show', $Suggestion->id)}}">View</a></td>
+            @auth
+                @if($Suggestion->getSubmitter->id !== Auth::user()->id)
+                    {{-- Check that the suggestion hasn't already been voted on by this user --}}
+                    @if(\App\Models\SuggestionVote::where('suggestion_id', $Suggestion->id)->where('user_id', Auth::user()->id))
+                    <td><a href="{{route('suggestion.show', $Suggestion->id)}}">&uarr;</a></td>
+                    <td><a href="{{route('suggestion.show', $Suggestion->id)}}">&darr;</a></td>
+                    @else
+                    <i>You have already voted on this suggestion</i>
+                    @endif
+                @endif
+            @endauth
         </tr>
     @endforeach
 </table>
